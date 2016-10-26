96ed1efaa4a1b6812721d01e4c615ee5064b3ba2
Anita Ilieva <anita.ilieva24@gmail.com>
2016-10-26 22:06:30 +0200

    Changed class' name to Question

---
+++ quiz/src/main/java/com/iliani14/pg6100/entity/Question.java
@@ -0,0 +1,84 @@
+package com.iliani14.pg6100.entity;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by anitailieva on 26/10/2016.
+ */
+
+@NamedQueries({
+        @NamedQuery(name = Question.GET_ALL_QUESTIONS, query = "SELECT q FROM Question q"),
+        @NamedQuery(name = Question.GET_QUESTION_BY_SUBSUBCATEGORY, query = "SELECT q FROM Question q WHERE q.subSubCategories.name = :name")
+})
+
+
+@Entity
+public class Question {
+
+        public static final String GET_ALL_QUESTIONS = "GET ALL QUESTIONS";
+        public static final String GET_QUESTION_BY_SUBSUBCATEGORY = "GET QUESTION BY SUBSUBCATEGORY";
+
+
+    @Id @GeneratedValue
+    private long id;
+
+    @NotNull
+    @Size(min = 1, max = 400)
+    private String text;
+
+    @NotNull
+    @Size(min = 1, max = 400)
+    private String theCorrectAnswer;
+
+
+    @ElementCollection
+    private List<String> answers;
+
+    @ManyToOne
+    private SubSubCategory subSubCategories;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public SubSubCategory getSubSubCategories(){
+        return subSubCategories;
+    }
+    public void setSubSubCategories(SubSubCategory subSubCategories){
+        this.subSubCategories = subSubCategories;
+    }
+
+    public String getText(){
+        return text;
+    }
+    public void setText(String text){
+        this.text = text;
+    }
+
+    public String getTheCorrectAnswer(){
+        return theCorrectAnswer;
+    }
+
+    public void setTheCorrectAnswer(String theCorrectAnswer){
+            this.theCorrectAnswer = theCorrectAnswer;
+}
+    public void setAnswers(List<String> answers){
+        this.answers = answers;
+        }
+
+        public List<String> getAnswers(){
+            if( answers == null){
+                return new ArrayList<>();
+            }
+            return answers;
+    }
+
+}
