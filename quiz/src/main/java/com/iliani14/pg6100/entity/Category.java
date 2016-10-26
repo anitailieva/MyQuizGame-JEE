88d54ce1f3e0ece68fa1082f51c4a1b603776978
Anita Ilieva <anita.ilieva24@gmail.com>
2016-10-26 11:54:53 +0200

    Added queries for Category

--- quiz/src/main/java/com/iliani14/pg6100/entity/Category.java
+++ quiz/src/main/java/com/iliani14/pg6100/entity/Category.java
@@ -1,9 +1,6 @@
 package com.iliani14.pg6100.entity;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
+import javax.persistence.*;
 import javax.validation.constraints.Size;
 import java.util.ArrayList;
 import java.util.List;
@@ -12,9 +9,18 @@ import java.util.List;
 /**
  * Created by anitailieva on 26/10/2016.
  */
+@NamedQueries({
+        @NamedQuery(name = Category.GET_ALL_CATEGORIES, query = "SELECT c FROM Category c"),
+        @NamedQuery(name = Category.GET_CATEGORY_BY_ID, query = "SELECT c FROM Category c WHERE c.id = :id")
+
+})
 
 @Entity
 public class Category {
+
+    public static final String GET_ALL_CATEGORIES = "GET ALL CATEGORIES";
+    public static final String GET_CATEGORY_BY_ID = "GET CATEGORY BY ID";
+
 
     @Id
     @Size(min = 1, max = 50)
