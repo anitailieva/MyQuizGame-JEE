03c63f50721a76cb1ee48001f6b1039959a2b4e6
Anita Ilieva <anita.ilieva24@gmail.com>
2016-11-03 21:22:57 +0100

    Removed categoryId that was a parameter

--- restApi/src/main/java/com/iliani14/pg6100/dto/SubSubCategoryDto.java
+++ restApi/src/main/java/com/iliani14/pg6100/dto/SubSubCategoryDto.java
@@ -12,21 +12,18 @@ public class SubSubCategoryDto {
     @ApiModelProperty("The id of the subsubcategory")
     public String id;
 
-    @ApiModelProperty("The id of the category subcategory")
-    public String categoryId;
 
     @ApiModelProperty("The id of the subcategory subcategory")
     public String subcategoryId;
 
+
     @ApiModelProperty("The name of the subsubcategory")
     public String name;
 
-
     public SubSubCategoryDto(){}
 
-    public SubSubCategoryDto(String id, String categoryId, String subcategoryId, String name){
+    public SubSubCategoryDto(String id, String subcategoryId, String name) {
         this.id = id;
-        this.categoryId = categoryId;
         this.subcategoryId = subcategoryId;
         this.name = name;
     }
