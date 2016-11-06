d4dc095325188bf91dc1e3d98a26feb8861bae75
Anita Ilieva <anita.ilieva24@gmail.com>
2016-11-06 17:43:07 +0100

    Created more tests for creating, retrieving, deleting and updating a subsubcategory

--- restApi/src/test/java/com/iliani14/pg6100/CategoryIT.java
+++ restApi/src/test/java/com/iliani14/pg6100/CategoryIT.java
@@ -1,13 +1,17 @@
+
 package com.iliani14.pg6100;
 
 import com.iliani14.pg6100.dto.CategoryDto;
+import com.iliani14.pg6100.dto.QuestionDto;
 import com.iliani14.pg6100.dto.SubCategoryDto;
+import com.iliani14.pg6100.dto.SubSubCategoryDto;
 import io.restassured.http.ContentType;
 import org.junit.Test;
 
+import java.util.List;
+
 import static io.restassured.RestAssured.*;
-import static org.hamcrest.Matchers.contains;
-import static org.hamcrest.Matchers.not;
+import static org.hamcrest.Matchers.*;
 import static org.hamcrest.core.Is.is;
 
 
@@ -27,15 +31,24 @@ public class CategoryIT extends CategoryTestBase {
     public void testGetAllCategories() {
         get().then().body("size()", is(0));
 
-        createSomeCategories();
+        String category1 = createCategory("Science");
+        String category2 = createCategory("Sports");
+        String category3 = createCategory("History");
 
-        get().then().body("size()", is(4));
+
+        get().then().body("size()", is(3));
+
+        given().get()
+                .then()
+                .statusCode(200)
+                .body("id", hasItems(category1, category2, category3))
+                .body("name", hasItems("Science", "Sports", "History"));
 
     }
 
     @Test
-    public void testCreateAndGet(){
-    String name = "Science";
+    public void testCreateAndGetCategory(){
+        String name = "Science";
         CategoryDto dto = new CategoryDto(null, name);
 
         get().then().statusCode(200).body("size()", is(0));
@@ -57,22 +70,6 @@ public class CategoryIT extends CategoryTestBase {
                 .body("name", is(name));
     }
 
-
-    private void createSomeCategories(){
-        createCategories("Science");
-        createCategories("Sports");
-        createCategories("History");
-        createCategories("Music");
-    }
-
-
-    private void createCategories(String name){
-        given().contentType(ContentType.JSON)
-                .body(new CategoryDto(null, name))
-                .post()
-                .then()
-                .statusCode(200);
-    }
     @Test
     public void testDeleteCategory(){
         String id = given().contentType(ContentType.JSON)
@@ -83,6 +80,7 @@ public class CategoryIT extends CategoryTestBase {
                 .extract().asString();
 
         get().then().body("id", contains(id));
+
         delete("/id/" + id);
 
         get().then().body("id", not(contains(id)));
@@ -127,30 +125,236 @@ public class CategoryIT extends CategoryTestBase {
 
         get("/id/" + id).then().body("name", is(anotherName));
     }
-    @Test
-    public void testCreateAndGetSubCategories() {
-        String name = "Sports";
-        CategoryDto dto = new CategoryDto(null, name);
 
-        get().then().statusCode(200).body("size()", is(0));
+
+    @Test
+    public void testGetAllSubcategories(){
+        get("/subcategories").then().body("size()", is(0));
+
+        String category = createCategory("Science");
+
+        String subcategory1 = createSubCategory(category, "Subcategory1");
+        String subcategory2 = createSubCategory(category, "Subcategory2");
+        String subcategory3 = createSubCategory(category, "Subcategory3");
+
+        get("/subcategories").then().body("size()", is(3));
+
+        given().get("/subcategories")
+                .then()
+                .statusCode(200)
+                .body("id", hasItems(subcategory1, subcategory2, subcategory3))
+                .body("name", hasItems("Subcategory1", "Subcategory2", "Subcategory3"));
+
+    }
+
+
+    @Test
+    public void testCreateAndGetSubCategory() {
+        String category = createParentCategory();
+        String subcategoryName = "Subcategory name";
+
+        SubCategoryDto dto = new SubCategoryDto(null, category, subcategoryName);
+
+        get("/subcategories").then().body("size()", is(0));
 
         String id = given().contentType(ContentType.JSON)
                 .body(dto)
-                .post()
+                .post("/subcategories")
                 .then()
                 .statusCode(200)
                 .extract().asString();
 
-        get().then().statusCode(200).body("size()", is(1));
+        get("/subcategories").then().body("size()", is(1));
 
+        given().pathParam("id", id)
+                .get("/subcategories/id/{id}")
+                .then()
+                .statusCode(200)
+                .body("id", is(id))
+                .body("name", is(subcategoryName));
 
-        String subName = "Computer Science";
-        given().contentType(ContentType.JSON)
-                .body(new SubCategoryDto(null, id, subName ))
+    }
+
+    @Test
+    public void testDeleteSubcategory(){
+        String id = given().contentType(ContentType.JSON)
+                .body(new SubCategoryDto(null, createParentCategory(), "SUB name"))
                 .post("/subcategories")
                 .then()
-                .statusCode(200);
+                .statusCode(200)
+                .extract().asString();
 
-        get(id + "/subcategories").then().body("size()", is(1));
+        get("/subcategories").then().body("id", contains(id));
+
+        delete("/subcategories/id/" + id);
+
+        get("/subcategories").then().body("id", not(contains(id)));
+
+
     }
+
+    @Test
+    public void updateSubcategory() throws Exception {
+        String name = "Name";
+
+        String id = given().contentType(ContentType.JSON)
+                .body(new SubCategoryDto(null, createParentCategory(), name))
+                .post("/subcategories")
+                .then()
+                .statusCode(200)
+                .extract().asString();
+
+        get("/subcategories/id/" + id).then().body("name", is(name));
+
+
+        String newName = "New name";
+
+        given().contentType(ContentType.TEXT)
+                .body(newName)
+                .pathParam("id", id)
+                .put("/subcategories/id/{id}/name")
+                .then()
+                .statusCode(204);
+
+        get("/subcategories/id/" + id).then().body("name", is(newName));
+
+    }
+
+    @Test
+    public void testGetAllSubSubcategories() {
+        String category = createCategory("Science");
+
+        String subcategory1 = createSubCategory(category, "Subcategory1");
+        String subcategory2 = createSubCategory(category, "Subcategory2");
+        String subcategory3 = createSubCategory(category, "Subcategory3");
+
+        String subsubcategory1 = createSubSubCategory(subcategory1, "Subsubcategory1");
+        String subsubcategory2 = createSubSubCategory(subcategory2, "Subsubcategory2");
+        String subsubcategory3 = createSubSubCategory(subcategory3, "Subsubcategory3");
+        String subsubcategory4 = createSubSubCategory(subcategory1, "Subsubcategory1");
+
+
+        get("/subsubcategories").then().body("size()", is(4));
+
+        given().get("/subsubcategories")
+                .then()
+                .statusCode(200)
+                .body("id", hasItems(subsubcategory1, subsubcategory2, subsubcategory3, subsubcategory4))
+                .body("name", hasItems("Subsubcategory1", "Subsubcategory2", "Subsubcategory3", "Subsubcategory1"));
+
+    }
+
+    @Test
+    public void testCreateAndGetSubSubCategory() {
+        String category = createCategory("Science");
+        String subcategory = createSubCategory(category, "Computer Science");
+
+        String name = "Java";
+
+        SubSubCategoryDto dto = new SubSubCategoryDto(null, subcategory, name);
+
+        get("subsubcategories").then().statusCode(200).body("size()", is(0));
+
+        String id = given().contentType(ContentType.JSON)
+                .body(dto)
+                .post("/subsubcategories")
+                .then()
+                .statusCode(200)
+                .extract().asString();
+
+        get("/subsubcategories").then().statusCode(200).body("size()", is(1));
+
+        given().pathParam("id", id)
+                .get("/subsubcategories/id/{id}")
+                .then()
+                .statusCode(200)
+                .body("id", is(id))
+                .body("name", is(name));
+    }
+
+    @Test
+    public void testDeleteSubSubCategory() {
+        String id = given().contentType(ContentType.JSON)
+                .body(new SubSubCategoryDto(null, createSubCategory(createParentCategory(), "SubCategory name"), "SubSubcategory name"))
+                .post("/subsubcategories")
+                .then()
+                .statusCode(200)
+                .extract().asString();
+
+        get("/subsubcategories").then().body("id", contains(id));
+
+        delete("/subsubcategories/id/" + id);
+
+        get("/subsubcategories").then().body("id", not(contains(id)));
+
+    }
+
+    @Test
+    public void testUpdateSubSubCategoryName() {
+        String name = "Subsubcategory name";
+
+        String id = given().contentType(ContentType.JSON)
+                .body(new SubSubCategoryDto(null, createSubCategory(createParentCategory(), "Subcategory name"), name))
+                .post("/subsubcategories")
+                .then()
+                .statusCode(200)
+                .extract().asString();
+
+        get("/subsubcategories/id/" + id).then().body("name", is(name));
+
+        String newName = "New name";
+
+        given().contentType(ContentType.TEXT)
+                .body(newName)
+                .pathParam("id", id)
+                .put("/subsubcategories/id/{id}/name")
+                .then()
+                .statusCode(204);
+
+        get("/subsubcategories/id/" + id).then().body("name", is(newName));
+    }
+
+
+    private String createCategory(String name){
+        return given().contentType(ContentType.JSON)
+                .body(new CategoryDto(null, name))
+                .post()
+                .then()
+                .statusCode(200)
+                .extract().asString();
+    }
+
+    private String createParentCategory() {
+        return createCategory("The parent category");
+    }
+
+    private String createSubCategory(String categoryId, String name) {
+        return given().contentType(ContentType.JSON)
+                .body(new SubCategoryDto(null, categoryId, name))
+                .post("/subcategories")
+                .then()
+                .statusCode(200)
+                .extract().asString();
+    }
+
+    private String createSubSubCategory(String subCategoryId, String name) {
+        return given().contentType(ContentType.JSON)
+                .body(new SubSubCategoryDto(null, subCategoryId, name))
+                .post("/subsubcategories")
+                .then()
+                .statusCode(200)
+                .extract().asString();
+    }
+
+    private String createQuestion(String subsubcategoryId, String question, List<String> answers, String correctAnswer) {
+        return given().contentType(ContentType.JSON)
+                .body(new QuestionDto(null, subsubcategoryId, question, answers, correctAnswer))
+                .post("/questions")
+                .then()
+                .statusCode(200)
+                .extract().asString();
+    }
+    
 }
+
+
