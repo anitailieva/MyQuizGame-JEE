2024631199c8d715ac522fa5d0fde7e5a352ac7a
Anita Ilieva <anita.ilieva24@gmail.com>
2016-11-08 11:34:41 +0100

    Added Formats interface

---
+++ restApi/src/main/java/com/iliani14/pg6100/api/Formats.java
@@ -0,0 +1,10 @@
+package com.iliani14.pg6100.api;
+
+/**
+ * Created by anitailieva on 08/11/2016.
+ */
+public interface Formats {
+    String BASE_JSON = "application/json; charset=UTF-8";
+    String V1_JSON = "application/vnd.pg6100.category+json; charset=UTF-8; version=1";
+
+}
