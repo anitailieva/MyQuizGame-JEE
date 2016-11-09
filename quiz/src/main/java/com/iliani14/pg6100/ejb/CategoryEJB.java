7b922148f78aada9c36f95f4e524bc10f357bf68
Anita Ilieva <anita.ilieva24@gmail.com>
2016-11-09 22:38:02 +0100

    Added methods retrieving a list of all quizzes and a list of subcategories for a category

--- quiz/src/main/java/com/iliani14/pg6100/ejb/CategoryEJB.java
+++ quiz/src/main/java/com/iliani14/pg6100/ejb/CategoryEJB.java
@@ -1,12 +1,17 @@
 package com.iliani14.pg6100.ejb;
 
 import com.iliani14.pg6100.entity.Category;
+import com.iliani14.pg6100.entity.Question;
+import com.iliani14.pg6100.entity.SubCategory;
+import com.iliani14.pg6100.entity.SubSubCategory;
 
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Created by anitailieva on 26/10/2016.
@@ -61,6 +66,27 @@ public class CategoryEJB {
                 category.setName(newName);
 
         }
+    }
+
+    public List<Category> getAllCategoriesWithAtLeastOneQuiz() {
+        List<Question> questions = em.createNamedQuery(Question.GET_ALL_QUESTIONS).getResultList();
+
+
+        return questions.stream().map(Question::getSubSubCategories).collect(Collectors.toList())
+                .stream().map(SubSubCategory::getSubCategories).collect(Collectors.toList())
+                .stream().map(SubCategory::getCategory).collect(Collectors.toList());
+    }
+
+    public List<SubCategory> getAllSubCategoriesForACategory(Long id) {
+        return new ArrayList<>(findCategoryById(id).getSubCategories());
+    }
+
+    public List<Question> getAllQuizzesForCategory(Long id){
+        Category category = em.find(Category.class, id);
+
+        if (category == null) throw new IllegalArgumentException("There is no category with the id: " + id);
+
+        return category.getListOfQuestions();
     }
 
 }
