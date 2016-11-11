98218b3c16940a3326aaed76f2105b1efe257948
Anita Ilieva <anita.ilieva24@gmail.com>
2016-11-11 21:14:39 +0100

    Changed version to 2

--- restApi/src/main/java/com/iliani14/pg6100/api/Formats.java
+++ restApi/src/main/java/com/iliani14/pg6100/api/Formats.java
@@ -5,6 +5,6 @@ package com.iliani14.pg6100.api;
  */
 public interface Formats {
     String BASE_JSON = "application/json; charset=UTF-8";
-    String V1_JSON = "application/vnd.pg6100.category+json; charset=UTF-8; version=2";
+    String V2_JSON = "application/merge-patch+json; charset=UTF-8; version=2";
 
 }
