bb318a6c434d8d15ec8a16e60d3f465704a07c8b
Anita Ilieva <anita.ilieva24@gmail.com>
2016-11-22 14:37:08 +0100

    Created an interface containing the formats used in the application

--- gameApi/src/main/java/com/iliani14/pg6100/api/Formats.java
+++ gameApi/src/main/java/com/iliani14/pg6100/api/Formats.java
@@ -5,7 +5,7 @@ package com.iliani14.pg6100.api;
  */
 public interface Formats {
 
-    String BASE_JSON = "application/json; charset=UTF-8";
+    String BASE_JSON = "application/json; charset=UTF-8; version=1" ;
     String V2_JSON = "application/merge-patch+json; charset=UTF-8; version=2";
 
 }
