6178db0470c06233d083b2880d44025754278d6f
Anita Ilieva <anita.ilieva24@gmail.com>
2016-11-22 14:34:30 +0100

    Create a Game entity

--- gameApi/src/main/java/com/iliani14/pg6100/Game.java
+++ gameApi/src/main/java/com/iliani14/pg6100/Game.java
@@ -1,4 +1,6 @@
-package com.iliani14.pg6100.entity;
+package com.iliani14.pg6100;
+
+import com.iliani14.pg6100.entity.Question;
 
 import javax.persistence.*;
 import java.util.ArrayList;
@@ -7,14 +9,20 @@ import java.util.List;
 /**
  * Created by anitailieva on 20/11/2016.
  */
+
+@NamedQuery(name = Game.GET_ALL_ACTIVE_GAMES, query = "SELECT g FROM Game g WHERE g.isActive = true")
 @Entity
 public class Game {
+
+        public static final String GET_ALL_ACTIVE_GAMES =  "GET ALL ACTIVE GAMES";
 
         @Id @GeneratedValue
         private Long id;
 
 
-        private int answer;
+        private int numberOfanswers;
+
+        private boolean isActive;
 
         @OneToMany(fetch = FetchType.EAGER)
         private List<Question> questions;
@@ -30,13 +38,22 @@ public class Game {
             this.id = id;
         }
 
-        public int getAnswer(){
-            return answer;
+        public int getNumberOfAnswers(){
+            return numberOfanswers;
         }
 
-        public void setAnswer(int answer){
-            this.answer = answer;
+        public void setNumberOfanswers(int numberOfanswers){
+            this.numberOfanswers = numberOfanswers;
         }
+
+        public boolean isActive(){
+           setActive(questions.size() == numberOfanswers);
+                return isActive;
+        }
+        public void setActive(boolean isActive){
+            isActive = isActive;
+            }
+
         public List<Question> getQuestions(){
             if(questions == null)
             return new ArrayList<>();
