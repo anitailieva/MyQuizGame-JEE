0ad336c49e1e2c034966d69499113293868dba0a
Anita Ilieva <anita.ilieva24@gmail.com>
2016-11-22 19:00:28 +0100

    Changed version to 1

--- gameApi/src/main/java/com/iliani14/pg6100/api/Formats.java
+++ gameApi/src/main/java/com/iliani14/pg6100/api/Formats.java
@@ -1,11 +1,12 @@
 package com.iliani14.pg6100.api;
 
+import javax.ws.rs.core.MediaType;
+
 /**
  * Created by anitailieva on 15/11/2016.
  */
 public interface Formats {
 
-    String BASE_JSON = "application/json; charset=UTF-8; version=1" ;
-    String V2_JSON = "application/merge-patch+json; charset=UTF-8; version=2";
+    String V1_JSON =  MediaType.APPLICATION_JSON + "; charset=UTF-8; version=1";
 
 }
