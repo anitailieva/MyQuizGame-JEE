57520e8503baf33b00e98f533b46ba1c66322d36
Anita Ilieva <anita.ilieva24@gmail.com>
2016-11-28 23:24:54 +0100

    Implemented methods for retrieving a game by id and all acitve games

--- gameApi/src/main/java/com/iliani14/pg6100/api/GameRestImpl.java
+++ gameApi/src/main/java/com/iliani14/pg6100/api/GameRestImpl.java
@@ -1,25 +1,63 @@
 package com.iliani14.pg6100.api;
 
-import com.google.common.base.Throwables;
+import com.iliani14.pg6100.Game;
+import com.iliani14.pg6100.dto.AnswerDto;
+import com.iliani14.pg6100.dto.GameConverter;
+import com.iliani14.pg6100.dto.GameDto;
+import io.swagger.annotations.ApiParam;
 
-import javax.validation.ConstraintViolationException;
-import javax.ws.rs.WebApplicationException;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.ws.rs.core.Response;
+import java.util.List;
 
 /**
  * Created by anitailieva on 15/11/2016.
  */
-public class QuizRestImpl{
+public class GameRestImpl implements GameRestApi{
+
+
+    private final String webAddress;
+
+    private EntityManagerFactory factory = Persistence.createEntityManagerFactory("DB");
+
+    private EntityManager em = factory.createEntityManager();
+
+    public GameRestImpl() {
+
+        webAddress = System.getProperty("quizApiAddress", "api.fixer.io");
+    }
 
 
 
+    @Override
+    public List<GameDto> getAllActiveGames() {
+        return GameConverter.transform(em.createNamedQuery(Game.GET_ALL_ACTIVE_GAMES).getResultList());
+    }
 
-    private WebApplicationException wrapException(Exception e) throws WebApplicationException {
-        Throwable cause = Throwables.getRootCause(e);
-        if (cause instanceof ConstraintViolationException) {
-            return new WebApplicationException("Invalid constraints on input: " + cause.getMessage(), 400);
-        } else {
-            return new WebApplicationException("Internal error", 500);
-        }
+    @Override
+    public Response createNewGame(@ApiParam("Number of quizzes in the game. 5 by default") String numberOfQuestions) {
+        return null;
+    }
+
+    @Override
+    public GameDto getGameById(@ApiParam("The id of the game") Long id) {
+        return GameConverter.transform(em.find(Game.class, id));
+    }
+
+    @Override
+    public AnswerDto answerTheQuestion(@ApiParam("The id of the game") Long id, @ApiParam("The answer to the question") String correctAnswer) {
+        return null;
+    }
+
+    @Override
+    public void endGame(@ApiParam("The id of the game") Long id) {
+        Game game = em.find(Game.class, id);
+        em.getTransaction().begin();
+        em.remove(game);
+        em.getTransaction().commit();
+
     }
 
 }
