cdcd163346e86211ba2f6aca9ceee79fc5c66005
Anita Ilieva <anita.ilieva24@gmail.com>
2016-11-30 22:00:23 +0100

    Added two more properties to category dto

--- quizApi/src/main/java/com/iliani14/pg6100/dto/CategoryDto.java
+++ quizApi/src/main/java/com/iliani14/pg6100/dto/CategoryDto.java
@@ -3,6 +3,8 @@ package com.iliani14.pg6100.dto;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 
+import java.util.List;
+
 /**
  * Created by anitailieva on 27/10/2016.
  */
@@ -24,10 +26,21 @@ public class CategoryDto {
     @ApiModelProperty("The name of the category")
     public String name;
 
-    public CategoryDto(){}
 
-    public CategoryDto(String id, String name){
+    @ApiModelProperty("Subcategories that belong to the category")
+    public List<SubCategoryDto> subcategories;
+
+    @ApiModelProperty("Specifying categories that belong to the category")
+    public List<SubSubCategoryDto> subSubCategories;
+
+    public CategoryDto() {
+    }
+
+    public CategoryDto(String id, String name, List<SubCategoryDto> subcategories,
+                    List<SubSubCategoryDto> subSubCategories) {
         this.id = id;
         this.name = name;
+        this.subcategories = subcategories;
+        this.subSubCategories = subSubCategories;
     }
 }
