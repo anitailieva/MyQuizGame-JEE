de831a3da105602121b3893e6a60351ff01db11a
Anita Ilieva <anita.ilieva24@gmail.com>
2016-11-30 21:59:22 +0100

    Added a method returning a collection

--- quizApi/src/main/java/com/iliani14/pg6100/dto/CategoryConverter.java
+++ quizApi/src/main/java/com/iliani14/pg6100/dto/CategoryConverter.java
@@ -1,7 +1,11 @@
 package com.iliani14.pg6100.dto;
 
+import com.iliani14.pg6100.dto.collection.ListDto;
 import com.iliani14.pg6100.entity.Category;
+import com.iliani14.pg6100.entity.SubCategory;
+import com.iliani14.pg6100.entity.SubSubCategory;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
@@ -27,11 +31,53 @@ public class CategoryConverter {
         return dto;
     }
 
-    public static List<CategoryDto> transform(List<Category> entities){
-        Objects.requireNonNull(entities);
+    public static CategoryDto transform(Category category, boolean expand) {
+        Objects.requireNonNull(category);
 
-        return entities.stream()
-                .map(CategoryConverter::transform)
-                .collect(Collectors.toList());
+        CategoryDto dto = new CategoryDto();
+        dto.id = String.valueOf(category.getId());
+        dto.name = category.getName();
+
+        if(expand) {
+            dto.subcategories = new ArrayList<>();
+            category.getSubCategories().stream()
+                    .map(SubCategoryConverter::transform)
+                    .forEach(subCategoryDto -> dto.subcategories.add(subCategoryDto));
+
+            dto.subSubCategories = new ArrayList<>();
+
+            List<SubCategory> subCategories = new ArrayList<>(category.getSubCategories());
+            for(SubCategory s : subCategories) {
+                for(SubSubCategory ssb : s.getSubSubCategories()){
+                    dto.subSubCategories.add(SubSubCategoryConverter.transform(ssb));
+
+                }
+            }
+        }
+
+        return dto;
+    }
+
+    public static ListDto<CategoryDto> transform(List<Category> categories, int offset,
+                                                 int limit, boolean expand){
+
+        List<CategoryDto> dtoList = null;
+        if(categories != null){
+            dtoList = categories.stream()
+                    .skip(offset) // this is a good example of how streams simplify coding
+                    .limit(limit)
+                    .map(category -> transform(category, expand))
+                    .collect(Collectors.toList());
+        }
+
+        ListDto<CategoryDto> dto = new ListDto<>();
+        dto.list = dtoList;
+        dto._links = new ListDto.ListLinks();
+        dto.rangeMin = offset;
+        assert dtoList != null;
+        dto.rangeMax = dto.rangeMin + dtoList.size() - 1;
+        dto.totalSize = categories.size();
+
+        return dto;
     }
 }
