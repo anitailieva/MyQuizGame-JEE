b4b6fddfd2e4551a6068703d0338af4481e128ac
Anita Ilieva <anita.ilieva24@gmail.com>
2016-12-03 23:10:34 +0100

    Changed basepath

--- gameApi/src/test/java/com/iliani14/pg6100/GameApplicationTest.java
+++ gameApi/src/test/java/com/iliani14/pg6100/GameApplicationTest.java
@@ -1,11 +1,9 @@
-import com.iliani14.pg6100.GameApplication;
-import com.iliani14.pg6100.GameConfiguration;
+package com.iliani14.pg6100;
+
 import io.dropwizard.testing.junit.DropwizardAppRule;
 import org.junit.ClassRule;
 
-/**
- * Created by anitailieva on 24/11/2016.
- */
+
 public class GameApplicationTest extends GameApplicationTestBase{
 
     @ClassRule
