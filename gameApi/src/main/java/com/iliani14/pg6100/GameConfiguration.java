ade170348fd9ac5c88ca7e13c0c3797246ecb7ac
Anita Ilieva <anita.ilieva24@gmail.com>
2016-12-03 23:12:12 +0100

    Setup for wiremock

--- gameApi/src/main/java/com/iliani14/pg6100/GameConfiguration.java
+++ gameApi/src/main/java/com/iliani14/pg6100/GameConfiguration.java
@@ -1,10 +1,7 @@
 package com.iliani14.pg6100;
 
-
 import io.dropwizard.Configuration;
 
-/**
- * Created by anitailieva on 15/11/2016.
- */
-public class GameConfiguration extends Configuration{
+
+public class GameConfiguration extends Configuration {
 }
